e526778dd31d71c1820eeee9108f29cf51205e39
MentaniPaulo <34354716+MentaniPaulo@users.noreply.github.com>
2022-01-30 23:39:16 -0300

    Update QTT_manager.h

--- src/QTT_manager.h
+++ src/QTT_manager.h
@@ -1,13 +1,13 @@
 #include <WiFi.h>
 #include <PubSubClient.h> //Biblioteca para as publicações via mqtt
-#define wifi "CLARO_2G02C36B"
-#define passwd "5A02C36B"
+#define wifi "YourWifi"
+#define passwd "passaword"
 
-#define TOPIC "/v1.6/devices/esp32"
-#define DEVICE_ID "61f6fd3c44371f113f58098c"
+#define TOPIC "/xxx/xxxxxxx/xxx"
+#define DEVICE_ID "xxxxxxxxxxxxxx"
 #define SERVER "things.ubidots.com"
 #define PORT 1883
-#define TOKEN "BBFF-ewhBSmqsadXqLTlpknFDfATV3ZhIl5"
+#define TOKEN "xxxxxxxxxxxxxxxxxxxxxxxx"
 
 void init();
 void reconnect();
